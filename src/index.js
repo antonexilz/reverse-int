99b920e5df0a4e946aa927a0c9ec38c0d1f94635
antonexilz <anton_mos@tut.by>
2022-01-05 10:42:52 +0300

    add: all passing in task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n.toFixed(0).split("").reverse();
+        n.pop(1);
+        return n.join("");
+    }
+
+    return n.toFixed(0).split("").reverse().join("");
+};
